04765ce2d7ce084668950a167b870872e2b1bb76
Kragline <armanaraqelyan1107@gmail.com>
2023-05-28 04:55:30 -0700

    comments for db functions

--- base.py
+++ base.py
@@ -3,6 +3,7 @@ import sqlite3
 
 
 def start_base():
+    """Function creates database"""
     global base, cursor
 
     base = sqlite3.connect('database.db')
@@ -13,12 +14,26 @@ def start_base():
 
 
 def sql_add(name : str, surname : str, age : str):
+    """Function addes new data to database
 
+    Args:
+        name (str): name of the person
+        surname (str): surname of the person
+        age (str): age of the person
+    """
     cursor.execute('INSERT INTO people VALUES(?, ?, ?)', (name.capitalize(), surname.capitalize(), age))
     base.commit()
 
 
-def sql_delete(member_number : int):
+def sql_delete(member_number : int) -> list:
+    """Function deletes person from db
+
+    Args:
+        member_number (int): Persons ID
+
+    Returns:
+        list: persons data
+    """
     results = cursor.execute('SELECT * FROM people').fetchall()
     for i in range(len(results)):
         if i == member_number:
@@ -30,18 +45,22 @@ def sql_delete(member_number : int):
     base.commit()
     
     new_list = []
-    new_list.append(del_name)
-    new_list.append(del_surname)
-    new_list.append(del_age)
+    new_list.append((del_name, del_surname, del_age))
 
     return new_list
 
 
-def sql_show():
+def sql_show() -> list:
+    """Function returns all data from db
+
+    Returns:
+        list: all data
+    """
     results = cursor.execute('SELECT * FROM people').fetchall()
     return results
 
 def delete_base():
+    """Function clears db and deletes it"""
     base.execute('DROP TABLE IF EXISTS people')
     base.commit()
     base.close()
